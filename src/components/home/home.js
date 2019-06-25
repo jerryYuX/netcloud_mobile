928abd4ea0afc038b0d86c964529fdd425d200d7
jurryYu <1342985759@qq.com>
2019-06-25 23:29:39 +0800

    'add'

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch,Redirect } from "react-router-dom";
+
 import user from "../user/user"
 import Login from "../login/login";
+import { Flex, WhiteSpace } from 'antd-mobile';
+import { Tabs, Badge } from 'antd-mobile';
+import PropTypes from 'prop-types';
+const tabs = [
+    { title: <Badge text={'3'}>推荐音乐</Badge>,key:1 },
+    { title: <Badge text={'今日(20)'}>热歌榜</Badge>,key:2 },
+    { title: <Badge dot>搜索</Badge>,key:3 },
+];
+const style = {display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', backgroundColor: '#fff'}
+
 class Home extends Component{
+    static contextTypes = {
+        router: PropTypes.object.isRequired,
+    }
+    renderContent = tab =>{
+        if(tab.key == 1){
+            return (<div style={style}>zujian1</div>)
+        }
+        if(tab.key == 2){
+            return (<div style={style}>zujian2</div>)
+        }
+        if(tab.key == 3){
+            return (<div style={style}>zujian3</div>)
+        }
+    };
+
     constructor(props) {
         super(props);
         this.state = {
@@ -14,24 +40,34 @@ class Home extends Component{
     }
     render() {
         const { match, location } = this.props;
-        console.log(match.url)
+
         return(
-            <div>
-                home
+            <div className="main-container">
 
-                <Route
-                    path={match.url}
-                    render={props => <div>test1</div>}
-                    exact
+                <Flex>
+                    <Flex.Item>
+                        <Tabs tabs={tabs}
+                              initialPage={1}
+                              onChange={(tab, index) => { console.log('onChange', index, tab); }}
+                              onTabClick={(tab, index) => {  }}
+                        >
+                            {this.renderContent}
+                        </Tabs>
 
-                    />
 
-                <Route
-                    path={`${match.url}/user`}
+                        <Route
+                            path={match.url}
+                            render={props => <div>test1</div>}
+                            exact
+                        />
+                        <Route
+                            path={`${match.url}/user`}
+                            component={user}
+                        />
 
+                    </Flex.Item>
 
-                    render={props => <div>test</div>}
-                     />
+                </Flex>
 
             </div>
 
