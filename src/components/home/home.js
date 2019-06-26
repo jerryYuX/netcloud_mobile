309ef3b3bd1835e94278cba0714ad2d29ce4188f
jerryYuX <1342985759@qq.com>
2019-06-26 10:56:57 +0800

    'ad'

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
+import 'antd-mobile/dist/antd-mobile.css';
+import './home.css'
 import { BrowserRouter as Router, Route, Switch,Redirect } from "react-router-dom";
-
+import { Flex, WhiteSpace ,Button} from 'antd-mobile';
+import { Tabs, Badge } from 'antd-mobile';
 import user from "../user/user"
 import Login from "../login/login";
-import { Flex, WhiteSpace } from 'antd-mobile';
-import { Tabs, Badge } from 'antd-mobile';
 import PropTypes from 'prop-types';
+import Header from '../header/header'
+import Hot_song from '../hotSong/hot_song'
+import Aplayer from '../player/Aplayer'
 const tabs = [
     { title: <Badge text={'3'}>推荐音乐</Badge>,key:1 },
     { title: <Badge text={'今日(20)'}>热歌榜</Badge>,key:2 },
     { title: <Badge dot>搜索</Badge>,key:3 },
 ];
-const style = {display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', backgroundColor: '#fff'}
+
+const style = { backgroundColor: '#fff'}
 
 class Home extends Component{
     static contextTypes = {
@@ -22,29 +27,41 @@ class Home extends Component{
             return (<div style={style}>zujian1</div>)
         }
         if(tab.key == 2){
-            return (<div style={style}>zujian2</div>)
+            return (<div style={style}><Hot_song/></div>)
         }
         if(tab.key == 3){
             return (<div style={style}>zujian3</div>)
         }
     };
 
+
+
     constructor(props) {
         super(props);
         this.state = {
-            username: "jack",
-            password: "123456",
-            redirectToReferrer: false   // 是否重定向到之前的页面
+            audio:{}
         };
 
     }
     render() {
         const { match, location } = this.props;
 
+
         return(
             <div className="main-container">
-
                 <Flex>
+                    <Flex.Item className={'header'}>
+                        <Header>
+
+                        </Header>
+
+
+
+                    </Flex.Item>
+
+                </Flex>
+
+                <Flex className={'toobars'}>
                     <Flex.Item>
                         <Tabs tabs={tabs}
                               initialPage={1}
@@ -68,11 +85,15 @@ class Home extends Component{
                     </Flex.Item>
 
                 </Flex>
+                <Aplayer audio={this.state.audio}/>
+
+
 
             </div>
 
         )
     }
+
 
 
 }
